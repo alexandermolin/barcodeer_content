cc99182dd04578c61386005436f63a5b9a98339d
alexandermolin <48133808+alexandermolin@users.noreply.github.com>
2022-02-03 13:08:46 +0100

    Update hello.R

--- hello.R
+++ hello.R
@@ -11,6 +11,7 @@ packageVersion("Seurat")
 
 library(readr)
 library(Seurat)
+library(SeuratObject)
 library(ggplot2)
 library(patchwork)
 
@@ -19,7 +20,7 @@ library(patchwork)
 # Note that this dataset also contains ~5% of mouse cells, which we can use as negative
 # controls for the protein measurements. For this reason, the gene expression matrix has
 # HUMAN_ or MOUSE_ appended to the beginning of each gene.
-cbmc.rna <- as.sparse(read.csv(file = "../GSE100866_CBMC_8K_13AB_10X-RNA_umi.csv.gz", sep = ",",
+cbmc.rna <- as.sparse(read.csv(file = "GSE100866_CBMC_8K_13AB_10X-RNA_umi.csv.gz", sep = ",",
     header = TRUE, row.names = 1))
 
 # To make life a bit easier going forward, we're going to discard all but the top 100 most
@@ -27,7 +28,7 @@ cbmc.rna <- as.sparse(read.csv(file = "../GSE100866_CBMC_8K_13AB_10X-RNA_umi.csv
 cbmc.rna <- CollapseSpeciesExpressionMatrix(cbmc.rna)
 
 # Load in the ADT UMI matrix
-cbmc.adt <- as.sparse(read.csv(file = "../GSE100866_CBMC_8K_13AB_10X-ADT_umi.csv.gz", sep = ",",
+cbmc.adt <- as.sparse(read.csv(file = "GSE100866_CBMC_8K_13AB_10X-ADT_umi.csv.gz", sep = ",",
     header = TRUE, row.names = 1))
 
 # Note that since measurements were made in the same cells, the two matrices have identical
